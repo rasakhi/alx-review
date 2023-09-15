54854473958e698e34ada14a5020912cf1aada41
rasakhi <michaelachumba@gmail.com>
2023-09-15 13:43:05 +0100

    C program that prints lowercase and uppercase alphabets

---
+++ alx-low_level_programming/0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase and uppercase alphabets
+ *
+ * Return: 0
+ */
+int main(void)
+{
+    char lower, upper;
+
+    lower = 'a';
+    upper = 'A';
+    while (lower <= 'z')
+    {
+        putchar(lower);
+        lower++;
+    }
+    while (upper <= 'Z')
+    {
+        putchar(upper);
+        upper++;
+    }
+    putchar('\n');
+
+    return (0);
+}
