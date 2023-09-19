4414e170bbb5fc9d0b638b1e826356c4fa146548
Michael Achumba <michaelachumba@gmail.com>
2023-09-19 19:01:49 +0100

    Program that adds 2 integers and returns the sum

---
+++ alx-low_level_programming/0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @i: first integer
+ * @j: second integer
+ * Return: Sum of i and j
+ */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	return (sum);
+}
