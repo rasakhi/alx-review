8a02aa24777c2f6dfd7082ff93e85292693559e9
Michael Achumba <michaelachumba@gmail.com>
2023-09-21 13:42:09 +0100

    Program that multiplies 2 integers

---
+++ alx-low_level_programming/0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: product of integers
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
