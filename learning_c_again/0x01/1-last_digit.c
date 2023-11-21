3c8c88d732d13b2220bf452401fbb15cd8667bc2
Michael Achumba <michaelachumba@gmail.com>
2023-11-21 07:27:18 -0800

    Program to print the last digit of a random number

---
+++ learning_c_again/0x01/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints the last digit of a number
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n, digit;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	digit = n % 10;
+	if (digit < 0)
+		digit *= -1;
+	if (digit > 5)
+		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
+	else if (digit == 0)
+		printf("Last digit of %d is %d and is 0\n", n, digit);
+	else
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
+	return (0);
+}
--- learning_c_again/0x01/README.md
+++ learning_c_again/0x01/README.md
@@ -1,3 +1,5 @@
 Variables, if, else, while Project
 
 Task 1 - Complete the source code in order to print whether the number stored in the variable n is positive, negative or zero.
+
+Task 2 - Complete the source code in order to print the last digit of the number stored in the variable n
